e83d1d381617634b67a89615c9dff195f49b1503
TrainingBasket <sourcecode@trainingbasket.in>
2020-06-01 12:39:53 +0530

    Added a url and view for home page with very basic template

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import tasks.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',tasks.views.home,name='home'),
 ]
